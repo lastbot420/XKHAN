862e028e2185de7c68cf225afd739f3a3a1f16cc
Mr85Tamim <148742606+Mr85Tamim@users.noreply.github.com>
2025-01-08 05:53:24 +0600

    Create dallepro.js

---
+++ scripts/cmds/dallepro.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const baseApiUrl = async () => {
+  const base = await axios.get(`https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`);
+  return base.data.api;
+}; 
+module.exports = {
+  config: {
+    name: "dalle",
+    aliases: ["bing", "create", "imagine"],
+    version: "1.0",
+    author: "Dipto",
+    countDown: 15,
+    role: 0,
+    description: "Generate images by Unofficial Dalle3",
+    category: "download",
+    guide: { en: "{pn} prompt" }
+  }, 
+  onStart: async({ api, event, args }) => {
+    const prompt = (event.messageReply?.body.split("dalle")[1] || args.join(" ")).trim();
+    if (!prompt) return api.sendMessage("❌| Wrong Format. ✅ | Use: 17/18 years old boy/girl watching football match on TV with 'Tamim' and '69' written on the back of their dress, 4k", event.threadID, event.messageID);
+    try {
+       //const cookies = "cookies here (_U value)";
+const cookies = ["1BW-8tBlHJv7nJ6kK2zrWykgbfOqsLnGoQNbCeg60Rocuui99ep45d28DHffSeqUTMpEzUSnjWVyiN0qmQLz3O3Nnivbo-JDw6AVrxLcAOtyJxYI4jaLnhmX026NRB5mKr4D2CsDTDOvHZt5iRYlm4I_fJMzGi1bfpPUTc4excaIVBoz3DkrlTTcL0cnE6RsklqLrbEOTg0sCSRKQJAKN9w"];
+const randomCookie = cookies[Math.floor(Math.random() * cookies.length)];
+      const wait = api.sendMessage("Wait koro baby 😽", event.threadID);
+      const response = await axios.get(`${await baseApiUrl()}/dalle?prompt=${prompt}&key=dipto008&cookies=${randomCookie}`);
+const imageUrls = response.data.imgUrls || [];
+      if (!imageUrls.length) return api.sendMessage("Empty response or no images generated.", event.threadID, event.messageID);
+      const images = await Promise.all(imageUrls.map(url => axios.get(url, { responseType: 'stream' }).then(res => res.data)));
+    api.unsendMessage(wait.messageID);
+   api.sendMessage({ body: `✅ | Here's Your Generated Photo 😘`, attachment: images }, event.threadID, event.messageID);
+    } catch (error) {
+      console.error(error);
+      api.sendMessage(`Generation failed!\nError: ${error.message}`, event.threadID, event.messageID);
+    }
+  }
+}
